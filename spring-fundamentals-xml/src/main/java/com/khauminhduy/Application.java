625bd9ebca9538ee01e185f7a1f5e982efa4aa69
khauminhduy <khauminhduy@gmail.com>
2022-06-12 18:27:38 +0700

    init config xml

---
+++ spring-fundamentals-xml/src/main/java/com/khauminhduy/Application.java
@@ -0,0 +1,17 @@
+package com.khauminhduy;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.khauminhduy.service.SpeakerService;
+import com.khauminhduy.service.SpeakerServiceImpl;
+
+public class Application {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		SpeakerService speakerService = applicationContext.getBean("speakerService", SpeakerService.class);
+		speakerService.findAll().forEach(System.out::println);
+	}
+
+}
---
+++ spring-fundamentals-xml/src/main/java/com/khauminhduy/service/SpeakerServiceImpl.java
@@ -0,0 +1,21 @@
+package com.khauminhduy.service;
+
+import java.util.List;
+
+import com.khauminhduy.model.Speaker;
+import com.khauminhduy.repository.HibernateSpeakerRepositoryImp;
+import com.khauminhduy.repository.SpeakerRepository;
+
+import lombok.Setter;
+
+@Setter
+public class SpeakerServiceImpl implements SpeakerService {
+
+	private SpeakerRepository speakerRepository = new HibernateSpeakerRepositoryImp();
+
+	@Override
+	public List<Speaker> findAll() {
+		return speakerRepository.findAll();
+	}
+
+}
